10f1419a98883b88409061a984dfe4ea3b1a220a
Nattasit Phosongchan <nattarsit@gmail.com>
2024-08-04 17:10:30 +0700

    first commit

---
+++ src/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>About page</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis asperiores totam perferendis nulla, impedit eligendi sit id. Maiores voluptatem expedita nesciunt atque alias. Placeat harum, dignissimos velit quaerat nam libero.</p>
+    </div>
+    
+  )
+}
+
+export default About
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './About';
+
+// Componenet function
+function App() {
+  return (
+    <div className="App">
+      <h1>App Component</h1>
+      <About />
+    </div>
+  );
+}
+
+// // Arror function
+// const App = () => <h1>Hello ReactJS</h1>
+
+export default App;
